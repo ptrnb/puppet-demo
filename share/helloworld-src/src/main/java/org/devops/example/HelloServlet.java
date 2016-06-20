5ffca4fb842dff566c8012b1c385462f5a849a6b
Peter Brown <peternbrown@mac.com>
2016-06-20 15:46:33 +1000

    Updated paths

--- share/helloworld-src/src/main/java/org/devops/example/HelloServlet.java
+++ share/helloworld-src/src/main/java/org/devops/example/HelloServlet.java
@@ -1,4 +1,4 @@
-package org.devops.test;
+package org.devops.example;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -17,26 +17,23 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/plain");
 		response.setStatus(200);
-		Properties props;
-        props = System.getProperties();   //get server JVM properties
+		Properties theProps;
+        theProps = System.getProperties();   
 		PrintWriter writer = response.getWriter();
 		writer.println("Hello from " + System.getenv("LOGNAME"));
         writer.println("-------- Server Java System properties ---------------") ;
 		if(request.getParameter("long")!=null){
-		    writer.println(" (long form listing)") ;
-			Enumeration enprop = props.propertyNames() ;
+			Enumeration enprop = theProps.propertyNames() ;
 			String key = "";
 			while ( enprop.hasMoreElements() ){
 				key = (String) enprop.nextElement() ;
-				writer.println(key+"\t"+props.getProperty(key)) ;
+				writer.println(key+"\t"+theProps.getProperty(key)) ;
 			}
             writer.close();
             return;
 		}
-        props.list(writer);  // abbreviated listing is default.
+        theProps.list(writer);  // abbreviated listing is default.
         writer.close(); 
 
-
-		/** writer.println("Hello from " + System.getenv("LOGNAME") + ", coming to us from: " + System.getenv("HOSTNAME")); **/
 	}
 }
