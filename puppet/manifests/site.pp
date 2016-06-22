35a5e2d4c00ee974caf391c55d4301d776cf6b21
Peter Brown <peternbrown@mac.com>
2016-06-22 22:10:47 +1000

    Basic working apache, tomcat and proxy_ajp

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,12 +1,43 @@
 node 'pnb.vagrant.example' {
 
-  class { 'java': }
+  class { 'java': 
+  }
 
-  include apache
+  class { 'apache':
+  }
 
-  apache::vhost { 'personal_site': 
-    port    => 80,
-    docroot => '/var/www/personal',
-    options => 'Indexes MultiViews',
+  class { 'apache::mod::proxy':
+  }
+
+  class { 'apache::mod::proxy_ajp': 
+  }
+
+  class { 'tomcat': 
+  }
+
+  tomcat::instance {'default':
+    install_from_source => false,
+    package_name        => [
+      'tomcat', 
+      'tomcat-webapps',
+      'tomcat-docs-webapp',
+      'tomcat-javadoc'],
+  }
+
+  tomcat::service { 'default':
+    use_jsvc     => false,
+    use_init     => true,
+    service_name => 'tomcat',
+  }
+
+  file { 'proxy_ajp.conf' :
+    path    => "/etc/httpd/conf.modules.d/proxy_ajp.conf",
+    ensure  => present,
+    source  => "/vagrant-share/config/proxy_ajp.conf",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '644',
+    notify  => Service['httpd'], # Apache will restart if this file is edited
+    require => Package['httpd'],
   }
 }
